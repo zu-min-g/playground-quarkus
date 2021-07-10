88257f45d235f6f9b9a4939cf92bc3e152c7f092
zu-min <68736447+zu-min-g@users.noreply.github.com>
2021-07-10 14:30:17 +0900

    first commit

---
+++ extension/deployment/src/main/java/com/zu_min/playground/quarkus/extension/deployment/ExtensionProcessor.java
@@ -0,0 +1,14 @@
+package com.zu_min.playground.quarkus.extension.deployment;
+
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+
+class ExtensionProcessor {
+
+    private static final String FEATURE = "extension";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+}
---
+++ main/src/test/java/com/zu_min/playground/quarkus/NativeReactiveGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.zu_min.playground.quarkus;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeReactiveGreetingResourceIT extends ReactiveGreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
