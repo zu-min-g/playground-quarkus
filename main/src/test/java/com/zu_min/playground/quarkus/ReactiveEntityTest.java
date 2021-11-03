a55a711099086e1eb7c470875234ed96bcd7b113
zu-min <68736447+zu-min-g@users.noreply.github.com>
2021-11-03 10:57:13 +0900

    Reactive Hibernate のテストでロールバックする方法の調査

---
+++ main/src/test/java/com/zu_min/playground/quarkus/ReactiveEntityTest.java
@@ -0,0 +1,171 @@
+package com.zu_min.playground.quarkus;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+
+import javax.inject.Inject;
+
+import com.zu_min.playground.quarkus.extension.runtime.Fruit;
+
+import org.hibernate.reactive.mutiny.Mutiny.Session;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.TestReactiveTransaction;
+import io.quarkus.test.TestTransaction;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.vertx.UniAsserter;
+import io.smallrye.mutiny.helpers.test.UniAssertSubscriber;
+
+@QuarkusTest
+class ReactiveEntityTest {
+
+    @Inject
+    Session session;
+
+    /**
+     * 通常の書き方（ロールバックしない）。
+     */
+    @Test
+    void testInsertAndCommit() {
+        var apple = new Fruit();
+        apple.name = "りんご";
+
+        var subscriber = apple.<Fruit>persist()
+
+            // DB へ反映
+            .call(e -> session.flush())
+
+            // 1 次キャッシュクリア
+            .invoke(e -> session.clear())
+
+            // 登録した情報を取得 （キャッシュを消したので DB から取得）
+            .chain(e -> Fruit.<Fruit>findById(e.id))
+            
+            // 購読者を指定して処理を実施
+            .subscribe().withSubscriber(UniAssertSubscriber.create());
+        
+        var actual = subscriber
+            // ブロックして結果を待つ
+            .awaitItem()
+            // 完了すること（例外が発生しないこと）
+            .assertCompleted()
+            // 結果を取得
+            .getItem();
+
+        // 結果の検証
+        assertEquals("りんご", actual.name);
+
+        // DB から再取得して新しいオブジェクト ID になっていることを確認
+        assertNotSame(System.identityHashCode(apple), System.identityHashCode(actual));
+    }
+
+    /**
+     * TestTransaction ではロールバックされない。
+     */
+    @Test
+    @TestTransaction
+    void testInsertWithTestTransaction() {
+        var apple = new Fruit();
+        apple.name = "りんご";
+
+        var subscriber = apple.<Fruit>persist()
+
+            // DB へ反映
+            .call(e -> session.flush())
+
+            // 1 次キャッシュクリア
+            .invoke(e -> session.clear())
+
+            // 登録した情報を取得 （キャッシュを消したので DB から取得）
+            .chain(e -> Fruit.<Fruit>findById(e.id))
+            
+            // 購読者を指定して処理を実施
+            .subscribe().withSubscriber(UniAssertSubscriber.create());
+        
+        var actual = subscriber
+            // ブロックして結果を待つ
+            .awaitItem()
+            // 完了すること（例外が発生しないこと）
+            .assertCompleted()
+            // 結果を取得
+            .getItem();
+
+        // 結果の検証
+        assertEquals("りんご", actual.name);
+
+        // DB から再取得して新しいオブジェクト ID になっていることを確認
+        assertNotSame(System.identityHashCode(apple), System.identityHashCode(actual));
+    }
+
+    /**
+     * TestReactiveTransaction の失敗例。例外が発生する。
+     */
+    @Test
+    @Disabled("失敗例")
+    @TestReactiveTransaction
+    void testFailureCaseOfInsertWithTestReactiveTransaction() {
+        var apple = new Fruit();
+        apple.name = "りんご";
+
+        var subscriber = apple.<Fruit>persist()
+
+            // DB へ反映
+            .call(e -> session.flush())
+
+            // 1 次キャッシュクリア
+            .invoke(e -> session.clear())
+
+            // 登録した情報を取得 （キャッシュを消したので DB から取得）
+            .chain(e -> Fruit.<Fruit>findById(e.id))
+            
+            // 購読者を指定して処理を実施
+            .subscribe().withSubscriber(UniAssertSubscriber.create());
+        
+        var actual = subscriber
+            // ブロックして結果を待つ
+            .awaitItem()
+            // 完了すること（例外が発生しないこと）
+            .assertCompleted()
+            // 結果を取得
+            .getItem();
+
+        // 結果の検証
+        assertEquals("りんご", actual.name);
+
+        // DB から再取得して新しいオブジェクト ID になっていることを確認
+        assertNotSame(System.identityHashCode(apple), System.identityHashCode(actual));
+    }
+
+    /**
+     * TestReactiveTransaction 書き換え例。
+     */
+    @Test
+    @TestReactiveTransaction
+    void testInsertWithTestReactiveTransactionAndUniAsserter(UniAsserter asserter) {
+        var apple = new Fruit();
+        apple.name = "りんご";
+        
+        asserter.assertThat(() -> {
+
+            return apple.<Fruit>persist()
+
+                // DB へ反映
+                .call(e -> session.flush())
+
+                // 1 次キャッシュクリア
+                .invoke(e -> session.clear())
+
+                // 登録した情報を取得 （キャッシュを消したので DB から取得）
+                .chain(e -> Fruit.<Fruit>findById(e.id));
+
+        }, actual -> {
+
+            // 結果の検証
+            assertEquals("りんご", actual.name);
+
+            // DB から再取得して新しいオブジェクト ID になっていることを確認
+            assertNotSame(System.identityHashCode(apple), System.identityHashCode(actual));
+        });
+    }
+}
