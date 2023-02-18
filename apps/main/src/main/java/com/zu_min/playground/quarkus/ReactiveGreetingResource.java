8c0154c97a2d9ace9a191c422a1ef5c400e7c22a
zu-min <68736447+zu-min-g@users.noreply.github.com>
2023-02-18 10:42:30 +0900

    Quarkus3 を導入してみる

--- apps/main/src/main/java/com/zu_min/playground/quarkus/ReactiveGreetingResource.java
+++ apps/main/src/main/java/com/zu_min/playground/quarkus/ReactiveGreetingResource.java
@@ -1,16 +1,18 @@
 package com.zu_min.playground.quarkus;
 
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import com.zu_min.playground.quarkus.extension.runtime.MyService;
 
 /**
  * サンプル REST API。
  */
+@ApplicationScoped
 @Path("/hello")
 public class ReactiveGreetingResource {
 
