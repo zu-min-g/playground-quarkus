9440c2f403aae0d6c8353f6592eed69e18fe595c
zu-min <68736447+zu-min-g@users.noreply.github.com>
2023-02-18 14:24:24 +0900

    Quarkus3 を導入してみる (#103)

--- extensions/extension/runtime/src/main/java/com/zu_min/playground/quarkus/extension/runtime/Fruit.java
+++ extensions/extension/runtime/src/main/java/com/zu_min/playground/quarkus/extension/runtime/Fruit.java
@@ -1,8 +1,8 @@
 package com.zu_min.playground.quarkus.extension.runtime;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
 
--- extensions/extension/runtime/src/main/java/com/zu_min/playground/quarkus/extension/runtime/MyService.java
+++ extensions/extension/runtime/src/main/java/com/zu_min/playground/quarkus/extension/runtime/MyService.java
@@ -1,6 +1,6 @@
 package com.zu_min.playground.quarkus.extension.runtime;
 
-import javax.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.ApplicationScoped;
 
 /**
  * サンプルサービス。
